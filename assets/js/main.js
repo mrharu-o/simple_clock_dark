937fde0c1919bf008b4e4bcc61fa6509743cd1d1
mrharu_o <33688387+mrharu-o@users.noreply.github.com>
2022-12-20 16:49:21 +0900

    added

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+function Date_get() {
+    var dates = new Date();
+    var years = dates.getFullYear();
+    var months = dates.getMonth()+1;
+    var days = dates.getDate();
+    var today = years + "年" + months + "月" + days + "日";
+    document.getElementById("Date_place").innerHTML = today;
+    }
+    setInterval('Date_get()',1000);//hoge
+
+function Time_get() {
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    var clock = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("Time_place").innerHTML = clock;
+    }
+    setInterval('Time_get()',1000);
